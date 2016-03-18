ec3626b785b04167fe95e08f73299026208c77e1
BRUNA <brutiko@gmail.com>
2016-03-18 02:32:58 +0100

    Fixing texts and related projects.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
 //= require_tree .
+
+$(document).ready(function() {
+    $('.navbar ul li a').each(function() {
+    if (this.href == window.location.href) {
+        $(this).addClass("active");
+        }
+    });
+});
+
+
